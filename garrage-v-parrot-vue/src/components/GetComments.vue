37dde5a3482c217603442e05bf72df60bcd595c5
Pierre-Cinart <cinartpierre@gmail.com>
2023-10-13 09:43:41 +0200

    amelioration css et ajout des champs nom et prenom au poste de commentaire

--- garrage-v-parrot-vue/src/components/GetComments.vue
+++ garrage-v-parrot-vue/src/components/GetComments.vue
@@ -12,12 +12,24 @@
     </div>
    
     <!-- Formulaire pour poster un commentaire -->
+    <h2>votre avis nous intéresse <br>
+    laissez nous un commentaire :</h2>
     <div class="post_comment">
-      <form @submit="postComment" method="post">
-        <textarea v-model="newComment" rows="5" placeholder="Votre commentaire"></textarea>
-        <button type="submit">Poster le commentaire</button>
-      </form>
-    </div>
+    <form @submit="postComment" method="post">
+      <div class="l">
+        <!-- Champ "Nom" -->
+      <input  type="text" v-model="newComment.nom" placeholder="Nom">
+      <br><br>
+      <!-- Champ "Prénom" -->
+      <input  type="text" v-model="newComment.prenom" placeholder="Prénom">
+      <br><br>
+      </div>
+      <!-- Champ "Commentaire" -->
+      <textarea v-model="newComment.text" rows="5" placeholder="Votre commentaire"></textarea>
+      
+      <button type="submit">Poster le commentaire</button>
+    </form>
+  </div>
   </div>
 </template>
 
@@ -49,11 +61,11 @@ export default {
     },
     postComment(event) {
       event.preventDefault();
-      // Envoyer le nouveau commentaire à votre API (à implémenter)
-      // Réinitialiser le champ du nouveau commentaire
-      this.newComment = '';
-      // Mettre à jour la liste des commentaires
-      this.getComments();
+      // // Envoyer le nouveau commentaire à votre API (à implémenter)
+      // // Réinitialiser le champ du nouveau commentaire
+      // this.newComment = '';
+      // // Mettre à jour la liste des commentaires
+      // this.getComments();
     },
     formatDate(date) {
       if (isDate(date)) {
@@ -66,5 +78,41 @@ export default {
 </script>
 
 <style scoped>
-/* Ajoutez vos styles CSS ici, s'ils sont spécifiques à ce composant */
+.post_comment {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+  background-color: #f9f9f9;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.post_comment textarea {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  resize: vertical;
+  margin-bottom: 10px;
+}
+
+.post_comment button {
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  background-color: #007bff;
+  color: #fff;
+  cursor: pointer;
+}
+.l {
+  text-align: left; /* Alignez le texte à gauche */
+}
+h2{
+  font-style: italic;
+  text-decoration: underline;
+  font-family: "Tilt Neon", sans-serif;
+  text-align: center;
+  margin-bottom: 20px;
+}
 </style>
