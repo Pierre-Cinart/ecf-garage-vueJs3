098d3e45cd085456cbc2cbc26b181862b388af29
Pierre-Cinart <cinartpierre@gmail.com>
2023-10-31 13:10:52 +0100

    pagination admin comments

--- admin/phpFunctions/showComments.php
+++ admin/phpFunctions/showComments.php
@@ -1,7 +1,7 @@
 <?php
 // Redirige vers la page de déconnexion si l'utilisateur n'est pas connecté
 if (!isset($_SESSION["log_in"]) || !isset($_SESSION["comments"])) {
-    header('Location:logout.php');
+    header('Location: logout.php');
     exit();
 }
 
@@ -15,12 +15,17 @@ $perPage = 5; // Nombre de commentaires par page
 // Calcule l'offset en fonction de la page actuelle
 $offset = ($currentPageShow - 1) * $perPage;
 
+// Requête SQL pour compter le nombre total de commentaires avec le statut correspondant
+$countQuery = "SELECT COUNT(*) FROM comments WHERE comment_status = '$commentsStatus'";
+$countResult = mysqli_query($bdd, $countQuery);
+$totalComments = mysqli_fetch_row($countResult)[0];
+
+// Calcule le nombre total de pages
+$totalPages = ceil($totalComments / $perPage);
+
 // Requête SQL pour récupérer les commentaires de la page actuelle
-$query = "SELECT * FROM comments WHERE comment_status = ? LIMIT ? OFFSET ?";
-$stmt = $bdd->prepare($query);
-$stmt->bind_param("sii", $commentsStatus, $perPage, $offset);
-$stmt->execute();
-$result = $stmt->get_result();
+$query = "SELECT * FROM comments WHERE comment_status = '$commentsStatus' LIMIT $perPage OFFSET $offset";
+$result = mysqli_query($bdd, $query);
 
 // Affiche les commentaires
 while ($row = $result->fetch_assoc()) {
@@ -28,8 +33,9 @@ while ($row = $result->fetch_assoc()) {
     echo '<div class="comment-box">';
     echo '<p class="comment-name">' . $row['firstname'] .' '. $row['lastname'] . '</p>';
     echo '<p class="comment-date"> le : ' . $row['comment_date'] . '</p>';
+    echo '<div class="comment-box">';
     echo '<p class="comment-text">' . $row['comment_text'] . '</p>';
-   
+    echo '</div>';
     echo '</div>';
 }
 
@@ -37,13 +43,24 @@ while ($row = $result->fetch_assoc()) {
 $previousPage = $currentPageShow - 1;
 $nextPage = $currentPageShow + 1;
 
+// Vérifie s'il y a une page précédente
+$hasPreviousPage = ($currentPageShow > 1);
+
+// Vérifie s'il y a une page suivante
+$hasNextPage = ($currentPageShow < $totalPages);
+
 echo '<div class="pagination">';
-if ($currentPageShow > 1) {
-    echo '<a href="showComments.php?commentsStatus=' . $commentsStatus . '&page=' . $previousPage . '">Page précédente</a>';
+if ($commentsStatus == "ok") {
+    $linkStatus = "ok=1";
+} elseif ($commentsStatus == "wait") {
+    $linkStatus = "wait=1";
+}
+if ($hasPreviousPage) {
+    echo '<a href="adminComments.php?' . $linkStatus . '&page=' . $previousPage . '">Page précédente</a>';
 }
 echo '<p>' . $currentPageShow . '</p>';
-if (mysqli_num_rows($result) == $perPage) {
-    echo '<a href="showComments.php?commentsStatus=' . $commentsStatus . '&page=' . $nextPage . '">Page suivante</a>';
+if ($hasNextPage) {
+    echo '<a href="adminComments.php?' . $linkStatus . '&page=' . $nextPage . '">Page suivante</a>';
 }
 echo '</div>';
 ?>
